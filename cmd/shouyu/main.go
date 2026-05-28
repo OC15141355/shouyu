dd4a3b651a4c8a8ebeaae5b47a2c7f8ac0825b02
ichiran <syrupface134@gmail.com>
2026-05-28 17:30:16 +1000

    chore: go module init + core deps (chi, go-oidc, templ, sqlite, yaml)

---
+++ cmd/shouyu/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+	log.Printf("shouyu listening on :%s", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OC15141355/shouyu
+
+go 1.26.2
